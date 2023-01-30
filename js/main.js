bc5274d5f056deeb50ad8655d3ab300c45763cdf
Beqa <123930902+Beqa-Nikoleishvili@users.noreply.github.com>
2023-01-30 12:48:16 +0400

    Main structure
    Main structure for further development.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Entry animation on site
+
+const obsrver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => obsrver.observe(el));
